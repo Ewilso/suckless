dd56a4611f141e17b5c92837ce979d4b021d3713
ewilso <17hughwilson@gmail.com>
2021-04-08 14:33:34 +0100

    Aperture rice

---
+++ Aperture/slock/config.h
@@ -0,0 +1,13 @@
+/* user and group to drop privileges to */
+static const char *user  = "wilson";
+static const char *group = "wilson";
+
+static const char *colorname[NUMCOLS] = {
+	[INIT] =   "black",     /* after initialization */
+	[INPUT] =  "#005577",   /* during input */
+	[FAILED] = "#CC3333",   /* wrong password */
+};
+
+/* treat a cleared input like a wrong password (color) */
+static const int failonclear = 1;
+static const int controlkeyclear = 0;
